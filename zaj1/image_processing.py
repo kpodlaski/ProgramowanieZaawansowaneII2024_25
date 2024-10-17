4e6d6f045452f6f3e8d432d501d5cf9582974ee7
kpodlaski <podlaski@uni.lodz.pl>
2024-10-17 16:23:15 +0200

    hsv

--- zaj1/image_processing.py
+++ zaj1/image_processing.py
@@ -9,7 +9,8 @@ blue = img[:,:,0]
 green = img[:,:,1]
 red = img[:,:,2]
 new_red = red.copy()
-new_red[new_red>200] = 0
+new_red[(new_red<50) & (new_red>30)] = 0
+img[:,:,2] = new_red
 
 plt.plot(np.histogram(blue,bins=256)[0], color="blue", label="blue")
 plt.plot(np.histogram(new_red,bins=256, range= (0,255))[0],
@@ -18,8 +19,23 @@ plt.plot(np.histogram(green,bins=256 )[0], color="green", label="green")
 plt.legend()
 plt.show()
 
+# plt.hist(blue,bins = 256)
+# plt.show()
+
 cv2.imshow("jez", img)
-cv2.imshow("jez_green", blue)
+cv2.imshow("jez_blue", blue)
+
+img_2 = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hue = img_2[:,:,0]
+saturation = img_2[:,:,1]
+value = img_2[:,:,2]
+
+cv2.imshow("jez_hue", hue)
+cv2.imshow("jez_sat", saturation)
+cv2.imshow("jez_v", value)
+
+
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
