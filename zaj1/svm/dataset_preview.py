5afba0f523ea6697dee5feda35cc263a457d73a0
kpodlaski <podlaski@uni.lodz.pl>
2024-11-21 14:12:50 +0100

    svm

--- zaj1/svm/dataset_preview.py
+++ zaj1/svm/dataset_preview.py
@@ -1,30 +1,97 @@
+import os
+from datetime import datetime
+from sklearn import svm
+from sklearn.metrics import confusion_matrix
+import json
+import pickle
+import seaborn as sb
 import torch
 import torchvision
+import numpy as np
 import matplotlib.pyplot as plt
 
+
 #pip install torch, torchvision
-base_path = "."
+base_path = "../"
 train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST(base_path+'/datasets/', train=True, download=True),
-    batch_size=1)
+    torchvision.datasets.MNIST(base_path+'datasets/', train=True, download=True,
+        transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               ])),
+    batch_size=60000, shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST(base_path+'/datasets/', train=False, download=True,
+    torchvision.datasets.MNIST(base_path+'datasets/', train=False, download=True,
                              transform=torchvision.transforms.Compose([
                                torchvision.transforms.ToTensor(),
                                ])),
-    batch_size = 10)
+    batch_size = 10000, shuffle=True)
+#Traing SVM\
 
-test_set = enumerate(test_loader)
+
+train_set = enumerate(train_loader)
+batch_idx, (train_data, train_targets) = next(train_set)
+train_data = train_data.reshape((60000,-1))
+print(train_data.shape)
+kernel = 'rbf' #linear, poly, sigmoid
+svm_file_path = base_path+'out/svm-model-{}.pkl'.format(kernel)
+
+# Check if the file exists
+if os.path.exists(svm_file_path):
+    with open(svm_file_path, 'rb') as f:
+        clf = pickle.load(f)
+        print("Loaded model from file")
+else:
+    start = datetime.now()
+    print("Start training:", start)
+    clf = svm.SVC(kernel=kernel)
+    clf.fit(train_data, train_targets)
+    end = datetime.now()
+    print("End training:", end)
+    print("Training time:",end-start)
+    # save svm as pickle
+    with open(svm_file_path, 'wb') as f:
+        pickle.dump(clf, f)
+    print("Saved model")
+    training_meta = {'method':'svm',
+                     'kernel':kernel,
+                     'file':os.path.basename(svm_file_path),
+                     'start':str(start),
+                     'end':str(end),
+                     'dataset':'mnist',
+                     'duration':str(end-start)}
+    with open(base_path + 'out/svm-train-metadata.json', 'r') as f:
+        _json = json.load(f)
+    _json['models'].append(training_meta)
+    with open(base_path+'out/svm-train-metadata.json', 'w') as f:
+        json.dump(_json, f, indent=4)
+
+
 examples = enumerate(test_loader)
 batch_idx, (example_data, example_targets) = next(examples)
+#print(example_data[0][0])
+print(example_data[0].shape)
+print(example_targets[0].shape)
+
+pred = clf.predict(example_data.reshape((10000,-1)))
 
 fig =plt.figure()
 for i in range(8):
       plt.subplot(2, 4, i + 1)
-      plt.tight_layout()
-      plt.imshow(example_data[i][0])
-      plt.title("Prediction: {}")
+      #plt.tight_layout()
+      plt.imshow(example_data[i][0], cmap='gray')
+
+      plt.title("P: {}, E: {} ".format(pred[i],example_targets[i]))
       plt.xticks([])
       plt.yticks([])
 fig.show()
+
+cmatrix= confusion_matrix(example_targets,pred)
+fig = plt.figure()
+sb.heatmap(cmatrix,annot =True, fmt='g')
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.title('SVM MNIST Confusion Matrix')
+fig.show()
+
+print("Accuracy:", np.trace(cmatrix)/np.sum(cmatrix))
